01d3e96c41bcc33c4812bbb94bf248c119bcfc04
purnama41 <purnama@41studio.com>
2017-12-27 10:08:36 +0700

    place order masih error

--- Gemfile
+++ Gemfile
@@ -52,3 +52,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'jquery-rails'
+gem 'jquery-ui-rails'
+gem 'bootstrap-sass', '~> 3.3.7'
+gem 'foreman'
+gem 'webpacker', github: "rails/webpacker"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :orders
+  resources :line_items
+  resources :carts
+  #get 'store/index'
+  root 'store#index', as: 'store_index'
+
+  resources :products do
+    get :who_bought, on: :member
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
