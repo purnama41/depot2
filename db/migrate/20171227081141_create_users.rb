b957f9ec698338357ebb6d1b92b881ecdb4f3ff3
purnama41 <purnama@41studio.com>
2018-01-02 13:47:16 +0700

    add status column to table users

--- db/migrate/20171227081141_create_users.rb
+++ db/migrate/20171227081141_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :password_digest
+      t.integer :status
 
       t.timestamps
     end
