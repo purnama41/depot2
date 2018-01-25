bf43162fc46a3be23d65fc8f338ac4542c79b6a7
purnama41 <purnama@41studio.com>
2018-01-25 09:39:12 +0700

    add action cable for realtime book price update

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,7 +3,7 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   def index
-    #@products = Product.order(:title)
-    @products = Product.page(params[:page]).per(3)
+    @products = Product.order(:title)
+    # @products = Product.page(params[:page]).per(3)
   end
 end
